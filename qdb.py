897c02d2e2e67f1998eff3992987dd66a81dd6a7
Edward Hades <edward.hades@gmail.com>
2012-03-31 19:28:24 +0200

    added license

--- qdb.py
+++ qdb.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python
+#
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# the accompanying COPYING file for more details.
+#
+
 import cherrypy
 import datetime
 import psycopg2
